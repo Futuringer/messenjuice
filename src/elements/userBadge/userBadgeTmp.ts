bd8a11fbfaadb20609cdfdc684d965a9eb0f335c
Fedorov Vladislav <futuringer.learning@gmail.com>
2023-04-15 13:26:40 +0300

    fix link

--- src/elements/userBadge/userBadgeTmp.ts
+++ src/elements/userBadge/userBadgeTmp.ts
@@ -1,6 +1,6 @@
 const userBadgeTmp = `
 <div class="userBadge__container">
-  <a href='/profile'><img class="userBadge__avatar" src={{avatar}} alt=''></a>
+  <a href='/settings'><img class="userBadge__avatar" src={{avatar}} alt=''></a>
   <div class="userBadge__name">{{name}}</div>
 </div>
 `;
