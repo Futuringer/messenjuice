fa310c1f9f04fafd0e4c1a501df61ffdc1b6cc5c
Fedorov Vladislav <futuringer.learning@gmail.com>
2023-04-25 12:21:44 +0300

    add avatar form component

---
+++ src/elements/avatarForm/avatarFormTmp.ts
@@ -0,0 +1,8 @@
+const avatarFormTmp = `
+<div class='avatarInput'>
+<label class="avatarInput__label" for='upload-avatar'>Change avatar</label>
+<input class='avatarInput__input' id="upload-avatar" type="file" name="myFile" accept="image/png, image/gif, image/jpeg" title=" "/>
+</div>
+`;
+
+export default avatarFormTmp;
---
+++ src/elements/avatarForm/index.ts
@@ -0,0 +1,21 @@
+import Block from '../../utils/block';
+import formTmp from './avatarFormTmp';
+
+type AvatarInputProps = {
+  events?: {
+    change?: (value: any) => void;
+  };
+};
+
+class AvatarInput extends Block {
+  constructor(props: AvatarInputProps) {
+    super(props);
+  }
+
+  render() {
+    const str = this.compile(formTmp, this.props);
+    return str;
+  }
+}
+
+export default AvatarInput;
