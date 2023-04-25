5e07ef21cb5df7fea256a6faaa442a2b936c7370
Fedorov Vladislav <futuringer.learning@gmail.com>
2023-04-25 12:31:36 +0300

    update profile page

--- src/pages/profile/index.ts
+++ src/pages/profile/index.ts
@@ -1,36 +1,114 @@
-import authController from '../../controllers/authController';
-import { withStore } from '../../utils/store';
+import AvatarForm from '../../elements/avatarForm';
+import store, { StoreEvents, withStore } from '../../utils/store';
+import { User } from '../../api/authApi';
 import { validateFormInput, setDefaultLabelState, handleInvalid, handleSubmitForm } from '../../utils/helpers';
-import { profileInputsConfig } from '../../utils/consts';
+import { profileInputsConfig, passwordsInputsConfig, infoInputsConfig } from '../../utils/consts';
 import InputWithLabel from '../../elements/inputWithLabel';
 import Form from '../../elements/form';
 import Input from '../../elements/input';
 import Button from '../../elements/button';
 import profileTmp from './profileTmp';
-import logo from '../../../static/imgs/avatarPlaceholder.png';
 import Block from '../../utils/block';
+import {
+  handleCancelClick,
+  handleAvatarChange,
+  handleProfileChangeSubmit,
+  handlePasswordChangeSubmit,
+  handleSignOutClick,
+} from './profile';
+import { eventBus } from '../../utils/eventBus';
 
 export type ProfilePageProps = {
+  data: User;
   name: string;
   img: string;
   form: typeof Form;
+  profileFormIsActive?: boolean;
+  passwordsFormIsActive?: boolean;
+  changeInfoFormIsActive: boolean;
+  avatarForm: AvatarForm;
 };
 
-const changeInfoButton = new Button({
+const profileChangeInfoButton = new Button({
+  variant: 'form',
+  text: 'Change Info',
+  type: 'submit',
+  isActive: true,
+  events: {
+    click: () => eventBus.emit('setProfileForm', 'changeInfo'),
+  },
+});
+
+const profileChangePasswordButton = new Button({
+  variant: 'form',
+  text: 'Change password',
+  type: 'button',
+  isActive: true,
+  events: {
+    click: () => eventBus.emit('setProfileForm', 'changePassword'),
+  },
+});
+
+const profileCancelButton = new Button({
+  variant: 'form',
+  text: 'Cancel',
+  type: 'button',
+  isActive: true,
+  events: {
+    click: handleCancelClick,
+  },
+});
+
+const profileSignOutButton = new Button({
+  variant: 'form',
+  text: 'Sign out',
+  type: 'button',
+  isActive: false,
+  events: {
+    click: handleSignOutClick,
+  },
+});
+
+const profileInputs = profileInputsConfig.map(item => {
+  return new InputWithLabel({
+    label: item.label,
+    input: new Input({
+      ...item,
+    }),
+  });
+});
+
+const profileForm = new Form({
+  formName: 'profileForm',
+  formText: 'Profile',
+  singleColumn: false,
+  buttons: [profileChangeInfoButton, profileChangePasswordButton, profileCancelButton, profileSignOutButton],
+  inputs: [...profileInputs],
+  events: {
+    submit: (e: HTMLFormElement) => {
+      handleSubmitForm(e, 'profileForm', () => console.log(12));
+    },
+  },
+});
+
+const passwordsChangeInfoButton = new Button({
   variant: 'form',
   text: 'Change Info',
   type: 'submit',
   isActive: true,
 });
 
-const cancelButton = new Button({
+const passwordsCancelButton = new Button({
   variant: 'form',
   text: 'Cancel',
   type: 'button',
   isActive: false,
+  events: {
+    click: () => eventBus.emit('setProfileForm', 'profile'),
+  },
 });
 
-const inputsWithLlabel = profileInputsConfig.map(item => {
+const passwordsInputs = passwordsInputsConfig.map(item => {
   return new InputWithLabel({
     label: item.label,
     input: new Input({
@@ -44,26 +122,108 @@ const inputsWithLlabel = profileInputsConfig.map(item => {
   });
 });
 
-const profileForm = new Form({
-  formName: 'profileForm',
-  formText: 'Profile',
+const passwordsForm = new Form({
+  formName: 'passwordsForm',
+  formText: 'Change password',
   singleColumn: false,
-  buttons: [changeInfoButton, cancelButton],
-  inputs: [...inputsWithLlabel],
+  buttons: [passwordsChangeInfoButton, passwordsCancelButton],
+  inputs: [...passwordsInputs],
   events: {
-    submit: (e: HTMLFormElement) => {
-      handleSubmitForm(e, 'profileForm', () => console.log(12));
-    },
+    submit: (e: HTMLFormElement) => handlePasswordChangeSubmit(e, 'passwordsForm'),
+  },
+});
+
+const infoChangeButton = new Button({
+  variant: 'form',
+  text: 'Change Info',
+  type: 'submit',
+  isActive: true,
+});
+
+const infoChangeCancelButton = new Button({
+  variant: 'form',
+  text: 'Cancel',
+  type: 'button',
+  isActive: false,
+  events: {
+    click: () => eventBus.emit('setProfileForm', 'profile'),
+  },
+});
+
+const infoChangeInputs = infoInputsConfig.map(item => {
+  return new InputWithLabel({
+    label: item.label,
+    input: new Input({
+      ...item,
+      events: {
+        blur: () => validateFormInput(item.name),
+        focus: () => setDefaultLabelState(item.name),
+        invalid: () => handleInvalid(item.name),
+      },
+    }),
+  });
+});
+
+const infoChangeForm = new Form({
+  formName: 'infoChangeForm',
+  formText: 'Change user information',
+  singleColumn: false,
+  buttons: [infoChangeButton, infoChangeCancelButton],
+  inputs: [...infoChangeInputs],
+  events: {
+    submit: (e: HTMLFormElement) => handleProfileChangeSubmit(e, 'infoChangeForm'),
+  },
+});
+
+const avatarForm = new AvatarForm({
+  events: {
+    change: handleAvatarChange,
   },
 });
 class ProfilePageComponent extends Block {
   constructor(props: ProfilePageProps) {
-    super({ ...props, name: 'Vlad', img: logo, form: profileForm });
+    super({
+      ...props,
+      profileForm,
+      passwordsForm,
+      infoChangeForm,
+      profileFormIsActive: true,
+      passwordsFormIsActive: false,
+      changeInfoFormIsActive: false,
+      avatarForm,
+    });
+    eventBus.on('setProfileForm', (value: 'profile' | 'changeInfo' | 'changePassword') => {
+      store.set('currentFormData.successText', '');
+      this.props.profileFormIsActive = false;
+      this.props.passwordsFormIsActive = false;
+      this.props.changeInfoFormIsActive = false;
+
+      switch (value) {
+        case 'changeInfo':
+          this.props.changeInfoFormIsActive = true;
+          break;
+        case 'changePassword':
+          this.props.passwordsFormIsActive = true;
+          break;
+        default:
+          this.props.profileFormIsActive = true;
+      }
+
+      store.set('currentFormData.errorText', '');
+    });
+
+    store.on(StoreEvents.Updated, () => {
+      this.props.avatar = `https://ya-praktikum.tech/api/v2/resources/${this.props.data.avatar}`;
+      (this.children.infoChangeForm as Block).props.errorText = store.getState().currentFormData.errorText;
+      (this.children.passwordsForm as Block).props.errorText = store.getState().currentFormData.errorText;
+      (this.children.infoChangeForm as Block).props.successText = store.getState().currentFormData.successText;
+      (this.children.passwordsForm as Block).props.successText = store.getState().currentFormData.successText;
+    });
   }
 
   init(): void {
-    // authController.fetchUser();
-    // console.log('this.props', this.props);
+    this.props.avatar = `https://ya-praktikum.tech/api/v2/resources/${this.props.data.avatar}`;
+    console.log('this.props', this.props);
   }
 
   render() {
---
+++ src/pages/profile/profile.ts
@@ -0,0 +1,36 @@
+import { handleSubmitForm } from '../../utils/helpers';
+import userController from '../../controllers/userController';
+import authController from '../../controllers/authController';
+import { ROUTES } from '../../index';
+import { router } from '../../utils/router';
+import { UpdateProfileData, UpdatePasswordData } from '../../api/userApi';
+
+export const handleCancelClick = () => {
+  router.go(ROUTES.MESSENGER);
+};
+
+export const handleAvatarChange = (e: any) => {
+  const formData = new FormData();
+  formData.append('avatar', e.target.files[0]);
+  userController.changeAvatar(formData);
+};
+
+export const handleProfileChangeSubmit = (e: HTMLFormElement, formName: string) => {
+  const callback = (data: UpdateProfileData) => {
+    userController.changeUserProfile(data);
+  };
+
+  handleSubmitForm(e, formName, callback);
+};
+
+export const handlePasswordChangeSubmit = (e: HTMLFormElement, formName: string) => {
+  const callback = (data: UpdatePasswordData) => {
+    userController.changeUserPassword(data);
+  };
+
+  handleSubmitForm(e, formName, callback);
+};
+
+export const handleSignOutClick = () => {
+  authController.logout();
+};
