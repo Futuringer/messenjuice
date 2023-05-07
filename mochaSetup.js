fd788548ec3335ffddbfc0392aea8f8fc54d86a4
Fedorov Vladislav <futuringer.learning@gmail.com>
2023-05-07 13:39:53 +0300

    add testing libraries

---
+++ mochaSetup.js
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { JSDOM } = require('jsdom');
+const Handlebars = require('handlebars');
+const fs = require('fs');
+
+const { window } = new JSDOM('<div id="root"></div>', {
+  url: 'http://localhost:3000',
+});
+
+global.window = window;
+global.document = window.document;
+global.DocumentFragment = window.DocumentFragment;
+
+require.extensions['.scss'] = function () {
+  module.exports = () => ({});
+};
+require.extensions['.png'] = () => null;
